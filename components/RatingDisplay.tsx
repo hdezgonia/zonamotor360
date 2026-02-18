99e8ac5bc8eaa5edc430aca56c9a6c337ae0a2ad
EV-MotorSport <96493431+hdezgonia@users.noreply.github.com>
2026-02-18 22:23:30 +0100

    Update RatingDisplay.tsx

--- components/RatingDisplay.tsx
+++ components/RatingDisplay.tsx
@@ -65,7 +65,7 @@ export function RatingDisplay({
               {label}
               {isHonorMatricula && (
                 <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-purple-600 rounded-full">
-                  M.H.
+                  Matricula de Honor
                 </span>
               )}
             </span>
