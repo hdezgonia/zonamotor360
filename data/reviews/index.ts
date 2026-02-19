b70adef8eb56edb4bbb3c6c2a37d7a3abdd73a29
EV-MotorSport <96493431+hdezgonia@users.noreply.github.com>
2026-02-19 09:55:12 +0100

    Update index.ts

--- data/reviews/index.ts
+++ data/reviews/index.ts
@@ -3,17 +3,32 @@ export * from "./utils"
 export * from "./types"
 
 import civicTypeR from "./civic_type_r.json"
+import cupraBorn from "./cupra_born.json"
+import cupraTavascan from "./cupra_tavascan.json"
+import dongfeng_box from "./dongfeng_box.json"
+import mazda3 from "./mazda_3.json"
 import mercedesB180D from "./mercedes-b180d-progressive.json"
-import polestar2Single from "./polestar-2-single-motor.json"
-import polestar2Performance from "./polestar-2-performance.json"
-import polestar4 from "./polestar-4.json"
+import polestar2Single from "./polestar_2_lrsm.json"
+import polestar2Performance from "./polestar_2_performance.json"
+import polestar4Single from "./polestar_4_lrsm.json"
+import polestar4Performance from "./polestar_4_performance.json"
+import xpengG6 from "./xpeng_g6.json"
+import xpengP7Wing from "./xpeng_p7_wing.json"
+
 
 export const reviews: CarReview[] = [
   civicTypeR,
+  cupraBorn,
+  cupraTavascan,
+  dongfeng_box,
+  mazda3,
   mercedesB180D,
   polestar2Single,
   polestar2Performance,
-  polestar4,
+  polestar4Single,
+  polestar4Performance,
+  xpengG6,
+  xpengP7Wing
 ].sort((a, b) => new Date(b.reviewDate).getTime() - new Date(a.reviewDate).getTime()) as CarReview[]
 
 export const getAllCategories = () => {
